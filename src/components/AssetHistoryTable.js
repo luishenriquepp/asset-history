8b2878b90593b4ee9798d2a32adca58dc7200f88
Luis Henrique Paim <luis.paim@poatek.com>
2021-05-26 10:16:36 -0300

    Hide history table when there are no registers

--- src/components/AssetHistoryTable.js
+++ src/components/AssetHistoryTable.js
@@ -1,27 +1,29 @@
 function AssetHistoryTable(props) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Action</th>
-          <th>Asset</th>
-          <th>Price</th>
-          <th>Amount</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.assets.map((asset) => (
-          <tr key={asset.id}>
-            <td>{asset.type}</td>
-            <td>{asset.asset}</td>
-            <td>{asset.price}</td>
-            <td>{asset.amount}</td>
-            <td>{asset.createdOn.toLocaleString()}</td>
+    props.assets.length > 0 && (
+      <table>
+        <thead>
+          <tr>
+            <th>Action</th>
+            <th>Asset</th>
+            <th>Price</th>
+            <th>Amount</th>
+            <th>Date</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.assets.map((asset) => (
+            <tr key={asset.id}>
+              <td>{asset.type}</td>
+              <td>{asset.asset}</td>
+              <td>{asset.price}</td>
+              <td>{asset.amount}</td>
+              <td>{asset.createdOn.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
   );
 }
 
